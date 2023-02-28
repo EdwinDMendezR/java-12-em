6c93c15c54dcd996e2355ba00035f9bd3d0c36af
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-02-28 10:08:00 -0500

    java-12: Indent

---
+++ src/test/java/news/method/strings/StringClassNewMethodsTest.java
@@ -0,0 +1,21 @@
+package news.method.strings;
+
+import org.junit.jupiter.api.Test;
+
+public class StringClassNewMethodsTest {
+
+    @Test
+    public void indenTest() {
+        String originalString = "java 7 \njava 8 \njava 9 \njava 10 \njava 11 \njava 12 \n";
+        String indentedString = originalString.indent(4);
+        System.out.println("String Indent:\n" + indentedString);
+        // "String Indent:
+        //            java 7
+        //            java 8
+        //            java 9
+        //            java 10
+        //            java 11
+        //            java 12
+    }
+
+}
